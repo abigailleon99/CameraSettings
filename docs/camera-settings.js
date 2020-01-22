1f3c5e3a3eab1a0cc4932d23d484e8e2f94026f7
Abigail Leon <abigailleon@dhcp92-125.calvin.edu>
2020-01-22 10:53:25 -0500

    Getting the image on to the screen

--- docs/camera-settings.js
+++ docs/camera-settings.js
@@ -1,7 +1,7 @@
 d3.select('h1')
     .style('color', 'darkseagreen')
 
-Caman("#legoMan", "http://127.0.0.1:5500/Lego.JPG", function () {
+Caman("#legoMan", "https://abigailleon99.github.io/CameraSettings/Lego.JPG", function () {
     // manipulate image here
   
   });
