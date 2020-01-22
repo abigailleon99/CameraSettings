059f45f10d6b0b4e09a92beafdd94b0e3f6ae301
Abigail Leon <abigailleon@dhcp92-125.calvin.edu>
2020-01-22 11:47:13 -0500

    brighter

--- docs/camera-settings.js
+++ docs/camera-settings.js
@@ -1,7 +1,7 @@
 d3.select('h1')
     .style('color', 'darkseagreen')
 
-Caman("#legoMan", "https://abigailleon99.github.io/CameraSettings/Lego.JPG", function () {
+Caman("#legoMan", "Lego.JPG", function () {
     // manipulate image here
-  
+    this.brightness(10).render();
   });
