cb9afc3b9b8894c38fb3f2abb1850db7d291a44a
Abigail Leon <abigailleon@dhcp92-125.calvin.edu>
2020-01-22 15:27:19 -0500

    idk

--- docs/camera-settings.js
+++ docs/camera-settings.js
@@ -3,5 +3,15 @@ d3.select('h1')
 
 Caman("#legoMan", "Lego.JPG", function () {
     // manipulate image here
-    this.brightness(10).render();
+    this.exposure(90).render();
+    this.brightness(50).render();
+    this.noise(50).render();
   });
+
+  // Make some buttons for each of the settings and then have them as the presets. 
+  // So now maybe make it seem like the brightness is doubling
+
+  //ISO will have to be the noise/ gain of an image. There will have to be a make up for it. 
+
+  // Shutter speed will have to be different. I will try to make it seem like the brightness isn't 
+  //the same as the aperature... but at the same time it will me 
