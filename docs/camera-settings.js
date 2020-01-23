326b6dcfd5ce199bb3b62702177206d11fc45393
Abigail Leon <abigailleon@dhcp92-125.calvin.edu>
2020-01-23 10:53:43 -0500

    edits

--- docs/camera-settings.js
+++ docs/camera-settings.js
@@ -1,12 +1,13 @@
 d3.select('h1')
     .style('color', 'black')
 
-Caman("#legoMan", "Lego.JPG", function () {
-    // manipulate image here
-    this.exposure(90).render();
-    this.brightness(50).render();
-    this.noise(50).render();
-  });
+// Caman("#legoMan", "Lego.JPG", function () {
+//     // manipulate image here
+//     this.exposure(90).render();
+//     this.brightness(100).render();
+//     this.noise(50).render();
+
+//   });
 
   // Make some buttons for each of the settings and then have them as the presets. 
   // So now maybe make it seem like the brightness is doubling
@@ -15,3 +16,13 @@ Caman("#legoMan", "Lego.JPG", function () {
 
   // Shutter speed will have to be different. I will try to make it seem like the brightness isn't 
   //the same as the aperature... but at the same time it will me 
+
+
+  // buttons to change things START HERE
+
+  d3.select("#f1").on('click', fstopOne)
+    function fstopOne(){
+        Caman('#legoMan', 'Lego.JPG', function(){
+            this.brightness(100).render();
+        }
+    }
