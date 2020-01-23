f36f904ce03af03a5bce4340311a8944c99ae3b6
Abigail Leon <abigailleon@dhcp92-125.calvin.edu>
2020-01-23 11:57:13 -0500

    things doe

--- docs/camera-settings.js
+++ docs/camera-settings.js
@@ -1,11 +1,18 @@
 d3.select('h1')
     .style('color', 'black')
 
+
+d3.select('#ISO').on('click', isoOne)
+function isoOne(){
+    console.log(this.value)
+    // this.noise(100).render();
+}
+    
 Caman("#legoMan", "Lego.JPG", function () {
     // manipulate image here
     this.exposure(100).render();
-    this.brightness(1).render();
-    this.noise(1).render();
+    // this.brightness(1).render();
+    // this.noise(1).render();
 
   });
 
