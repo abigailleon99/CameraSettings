d1dbbd6dcc31ebcb7f87e03f9a8dcbd2d2ab739f
Abigail Leon <abigailleon@dhcp92-125.calvin.edu>
2020-01-24 11:29:28 -0500

    working on buttons in css

--- docs/camera-settings.js
+++ docs/camera-settings.js
@@ -2,24 +2,21 @@ d3.select('h1')
     .style('color', 'black')
 
 
-d3.select('#ISO').on('click', isoOne)
-function isoOne(){
-    console.log(this.value)
-    // this.noise(100).render();
-}
-    
 Caman("#legoMan", "Lego.JPG", function () {
-    // manipulate image here
-    this.exposure(100).render();
+    //manipulate image here 
+    // this.exposure(100).render();
     // this.brightness(1).render();
     this.noise(100).render();
 
+    // d3.select('#ISO').on('click', isoOne)
+    //     function isoOne(){
+    //     // console.log(this.value)
+    //     this.noise(100).render();
+// }
   });
 
-//   d3.select("#f1").on('click', fstopOne)
-
-//     function fstopOne(){
-//         Caman('#legoMan', 'Lego.JPG', function(){
-//             this.brightness(100).render();
-//         }
-//     }
+  const buttons = d3.selectAll('input');
+    buttons.on('change', function(d) {
+        console.log('button changed to ' + this.value);
+    });    
+    
