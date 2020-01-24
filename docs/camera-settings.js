1e281efea48f6d33e7dd47a1434f3315ef35d0f8
Abigail Leon <abigailleon@dhcp92-125.calvin.edu>
2020-01-24 11:57:30 -0500

    isolating radio buttons in css

--- docs/camera-settings.js
+++ docs/camera-settings.js
@@ -6,17 +6,45 @@ Caman("#legoMan", "Lego.JPG", function () {
     //manipulate image here 
     // this.exposure(100).render();
     // this.brightness(1).render();
-    this.noise(100).render();
+    // this.noise(100).render();
 
-    // d3.select('#ISO').on('click', isoOne)
-    //     function isoOne(){
-    //     // console.log(this.value)
-    //     this.noise(100).render();
-// }
+
+  //ISO BUTTON
+  const isoButton = d3.selectAll('input[name=ISO]');
+  isoButton.on('change', function(d) {
+        console.log('ISO changed to ' + this.value);
+
+        Caman("#legoMan", "Lego.JPG", function () {
+            this.noise(10).render();
+
+        });
+
+    })
+
+    //APERTURE BUTTON
+    const apertureButton = d3.selectAll('input[name=F-Stop]');
+    apertureButton.on('change', function(d) {
+        console.log('Aperture changed to ' + this.value);
+
+        Caman("#legoMan", "Lego.JPG", function () {
+            this.noise(10).render();
+
+        });
+
+    })
+
+    //SHUTTER SPEED BUTTON
+    const shuttterButton = d3.selectAll('input[name=shutterSpeed]');
+    shuttterButton.on('change', function(d) {
+        console.log('Shutter Speed changed to ' + this.value);
+
+        Caman("#legoMan", "Lego.JPG", function () {
+            this.noise(10).render();
+
+        });
+
+    })
+
   });
-
-  const buttons = d3.selectAll('input');
-    buttons.on('change', function(d) {
-        console.log('button changed to ' + this.value);
-    });    
+ 
     
